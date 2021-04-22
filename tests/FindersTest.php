311de3774fcde86e3711cf5849e3e22a97111ddb
Breno Roosevelt <breno.jesus@ufms.br>
2021-04-22 09:29:58 -0400

    test finders

---
+++ tests/FindersTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace FlexFqcnFinder\Test;
+
+use FlexFqcnFinder\Finder\ComposerClassMap;
+use FlexFqcnFinder\Finder\GetDeclaredClasses;
+use FlexFqcnFinder\Finder\GetDeclaredInterfaces;
+use FlexFqcnFinder\Finder\GetDeclaredTraits;
+
+class FindersTest extends TestCase
+{
+    public function testGetDeclaredClasses()
+    {
+        $finder = new GetDeclaredClasses();
+        $this->assertEquals(get_declared_classes(), $finder->find());
+    }
+
+    public function testGetDeclaredTraits()
+    {
+        $finder = new GetDeclaredTraits();
+        $this->assertEquals(get_declared_traits(), $finder->find());
+    }
+
+    public function testGetDeclaredInterfaces()
+    {
+        $finder = new GetDeclaredInterfaces();
+        $this->assertEquals(get_declared_interfaces(), $finder->find());
+    }
+
+    public function testComposerClassMap()
+    {
+        $finder = new ComposerClassMap(__DIR__ . DS . ".." . DS . "vendor" . DS . "autoload.php" );
+        $this->assertNotEmpty($finder->find());
+    }
+}
